02912b7ddb273a94ab90b521b0fa3121cf8047e2
budi3312101075 <budiprayoga5103@gmail.com>
2024-08-25 13:14:24 +0700

    fixed fitur news

--- controllers/news.js
+++ controllers/news.js
@@ -4,7 +4,7 @@ import { uuid, dateValue } from "../utils/tools.cjs";
 export const getNews = async (req, res) => {
   try {
     const data = await query(
-      `SELECT n.uuid, n.title, n.description, n.photo, n.created_At, e.name FROM 
+      `SELECT n.uuid, n.title, n.content, n.created_At, e.name FROM 
       news n INNER JOIN user u ON n.id_user = u.uuid 
       INNER JOIN employee e ON e.uuid = u.id_employee WHERE n.is_deleted = 0`
     );
@@ -15,32 +15,37 @@ export const getNews = async (req, res) => {
   }
 };
 
+export const getNewsById = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const [data] = await query(
+      `SELECT n.uuid, n.title, n.content, n.created_At, e.name FROM 
+      news n INNER JOIN user u ON n.id_user = u.uuid 
+      INNER JOIN employee e ON e.uuid = u.id_employee WHERE n.is_deleted = 0 AND n.uuid = ?`,
+      [id]
+    );
+    return res.status(200).json(data);
+  } catch (error) {
+    return res.status(400).json({ message: error.message });
+  }
+};
+
 export const addNews = async (req, res) => {
-  const { title, description } = req.body;
+  const { title, content } = req.body;
   const userId = req.user.uuid;
   try {
     if (
       title === "" ||
-      description === "" ||
+      content === "" ||
       title === undefined ||
-      description === undefined
+      content === undefined
     ) {
       return res.status(400).json("Invalid data!");
     }
 
-    if (req.file === undefined) {
-      return res.status(400).json({ msg: "No file uploaded" });
-    }
-
-    const { filename: photo, size } = req.file;
-    const fileSize = size;
-    if (fileSize > 5000000) {
-      return res.status(422).json({ msg: "Image must be smaller than 5MB" });
-    }
-
     await query(
-      `INSERT INTO news (uuid, title, description, photo, is_deleted, id_user, created_at, updated_at) VALUES (?, ?, ?, ?, ?, ?, ?, ?)`,
-      [uuid(), title, description, photo, 0, userId, dateValue(), dateValue()]
+      `INSERT INTO news (uuid, title, content,  is_deleted, id_user, created_at, updated_at) VALUES (?, ?, ?, ?, ?, ?, ?)`,
+      [uuid(), title, content, 0, userId, dateValue(), dateValue()]
     );
 
     return res.status(200).json({ message: "data successfully added" });
@@ -50,40 +55,25 @@ export const addNews = async (req, res) => {
 };
 
 export const updateNews = async (req, res) => {
-  const { title, description } = req.body;
+  const { title, content } = req.body;
   const userId = req.user.uuid;
   const { id } = req.params;
   try {
     if (
       title === "" ||
-      description === "" ||
+      content === "" ||
       title === undefined ||
-      description === undefined
+      content === undefined
     ) {
       return res.status(400).json("Invalid data!");
     }
 
-    if (req.file === undefined) {
-      await query(
-        `UPDATE news SET title = ?, description = ?, id_user = ?, updated_at = ? WHERE uuid = ?`,
-        [title, description, userId, dateValue(), id]
-      );
+    await query(
+      `UPDATE news SET title = ?, content = ?, id_user = ?, updated_at = ? WHERE uuid = ?`,
+      [title, content, userId, dateValue(), id]
+    );
 
-      return res.status(200).json({ message: "data successfully updated" });
-    } else {
-      const { filename: photo, size } = req.file;
-      const fileSize = size;
-      if (fileSize > 5000000) {
-        return res.status(422).json({ msg: "Image must be smaller than 5MB" });
-      }
-
-      await query(
-        `UPDATE news SET title = ?, description = ?, photo = ?, id_user = ?, updated_at = ? WHERE uuid = ?`,
-        [title, description, photo, userId, dateValue(), id]
-      );
-
-      return res.status(200).json({ message: "data successfully updated" });
-    }
+    return res.status(200).json({ message: "data successfully updated" });
   } catch (error) {
     return res.status(400).json({ message: error.message });
   }
--- routes/api/news.js
+++ routes/api/news.js
@@ -3,16 +3,17 @@ import {
   addNews,
   deleteNews,
   getNews,
+  getNewsById,
   updateNews,
 } from "../../controllers/news.js";
 import { privateRoutes } from "../../middleware/private.js";
-import multer from "../../middleware/multer.js";
 
 const router = express.Router();
 
 router.get("/news", privateRoutes, getNews);
-router.post("/news", privateRoutes, multer, addNews);
-router.patch("/news/:id", privateRoutes, multer, updateNews);
+router.get("/news/:id", privateRoutes, getNewsById);
+router.post("/news", privateRoutes, addNews);
+router.patch("/news/:id", privateRoutes, updateNews);
 router.delete("/news/:id", privateRoutes, deleteNews);
 
 export default router;
